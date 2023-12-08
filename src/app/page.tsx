2981f3aea99362d7c465560ff7419545a09c6dd6
KoJaco <kojaco.dev@gmail.com>
2023-12-08 16:44:33 +1100

    feat: implement basic light/dark mode toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,34 +3,43 @@ import Link from "next/link";
 import { CreatePost } from "~/app/_components/create-post";
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
+import { ModeToggle } from "./_components/theme/mode-toggle";
 
 export default async function Home() {
   const hello = await api.post.hello.query({ text: "from tRPC" });
   const session = await getServerAuthSession();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-neutral-800 to-neutral-900 text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
-          </p>
-          <div className="flex flex-col items-center justify-center gap-4">
-            <p className="text-center text-2xl text-white">
-              {session && <span>Logged in as {session.user?.name}</span>}
-            </p>
-            <Link
-              href={session ? "/api/auth/signout" : "/api/auth/signin"}
-              className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-            >
-              {session ? "Sign out" : "Sign in"}
-            </Link>
-          </div>
+    <>
+      <header className="top0 absolute flex w-full justify-between px-4 py-8">
+        {/* theme toggle */}
+        <div className="ml-auto">
+          <ModeToggle />
         </div>
+      </header>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-neutral-50 to-neutral-200 text-neutral-900 dark:from-neutral-800 dark:to-neutral-900 dark:text-white">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+          <div className="flex flex-col items-center gap-2">
+            <p className="text-2xl text-neutral-900 dark:text-white">
+              {hello ? hello.greeting : "Loading tRPC query..."}
+            </p>
+            <div className="flex flex-col items-center justify-center gap-4">
+              <p className="text-center text-2xl text-neutral-900 dark:text-white">
+                {session && <span>Logged in as {session.user?.name}</span>}
+              </p>
+              <Link
+                href={session ? "/api/auth/signout" : "/api/auth/signin"}
+                className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+              >
+                {session ? "Sign out" : "Sign in"}
+              </Link>
+            </div>
+          </div>
 
-        <CrudShowcase />
-      </div>
-    </main>
+          <CrudShowcase />
+        </div>
+      </main>
+    </>
   );
 }
 
@@ -51,16 +60,4 @@ async function CrudShowcase() {
       <CreatePost />
     </div>
   );
-}
-
-async function generateImage() {
-  await new Promise((resolve) => setTimeout(resolve, 500));
-
-  const res = await fetch("/api/generate", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ prompt: "" }),
-  });
 }
